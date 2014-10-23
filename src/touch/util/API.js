b17804d21087fda15e9ed3bc510e0487040e4cb0
Chris Alfano <chris@jarv.us>
2014-10-23 19:42:43 -0400

    added singleton for proxy default

---
+++ src/touch/util/API.js
@@ -0,0 +1,10 @@
+/* jshint undef: true, unused: true, browser: true, quotmark: single, curly: true */
+/* global Ext */
+
+/**
+ * A default singleton instance for {@link Emergence.util.AbstractAPI}
+ */
+Ext.define('Emergence.touch.util.API', {
+    extend: 'Emergence.touch.util.AbstractAPI',
+    singleton: true
+});
