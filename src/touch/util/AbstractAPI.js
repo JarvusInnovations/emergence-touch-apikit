99b391dee56d77959f69fca15360f790fc056885
Kevin Clough <kevinclough@gmail.com>
2015-01-13 16:26:29 -0500

    fix fn name overlap

--- src/touch/util/AbstractAPI.js
+++ src/touch/util/AbstractAPI.js
@@ -129,7 +129,7 @@ Ext.define('Emergence.touch.util.AbstractAPI', {
      * @param {Function} callback A function to call and pass the new client data to when it is available
      * @param {Object} scope Scope for the callback function
      */
-    getSessionData: function(callback, scope) {
+    loadSessionData: function(callback, scope) {
         var me = this;
 
         me.request({
